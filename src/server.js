3b81fb5d4d2dd98f94e6451b39797ef783f9639a
neelop1204 <200556051@student.georgianc.on.ca>
2024-11-25 18:43:34 -0500

    ew

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
